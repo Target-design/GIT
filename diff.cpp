27ca9d57c72d98a2a107adc96c77558c9fb32479
Shreyas <kulkarnishreyas7868@gmail.com>
2021-08-02 18:12:18 +0530

    The implementation for diff.cpp is now added

--- diff.cpp
+++ diff.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int difference(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
+    int a = 10;
+    int b = 5;
+    cout << difference(a, b);
 }
